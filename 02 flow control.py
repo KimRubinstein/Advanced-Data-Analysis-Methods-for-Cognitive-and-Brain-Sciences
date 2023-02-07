ba0f829dafd73a83652ba6516218c90905126653
KimRubinstein <kimru@post.bgu.ac.il>
2023-02-07 21:54:27 +0200

    adding

--- 02 flow control.py
+++ 02 flow control.py
@@ -111,5 +111,34 @@ else:               # the "else" keyword in a for loop specifies a block of code
 
 # %%
 # "while" loops
+# while loop execute a set of statements as long as a condition is true
+# (origin :https://www.w3schools.com/python/python_while_loops.asp)
 
+i = 1
+while i < 6:
+  print(i)
+  i += 1        # why do we need to increment i?
 
+# %%
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break       # using "break" we can stop the loop even if the while condition is true
+  i += 1
+
+# %%
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue        # using "continue" we can stop the current iteration, and continue with the next
+  print(i)
+
+# %%
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:       # using "else" we can run a block of code once when the condition no longer is true
+  print("i is no longer less than 6")
