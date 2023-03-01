1765306faa1302116c9ef263f0905e295817ad49
Tal Golan <talgolan@Tals-MacBook-Pro.local>
2023-03-01 11:08:29 +0200

    slight modifications to 01_basics

--- 01_intro/01_basics.py
+++ 01_intro/01_basics.py
@@ -1,4 +1,4 @@
-# lessons 01-03 are based on Mattan S. Ben-Shachar 
+# lessons 01-03 are inspired by Mattan S. Ben-Shachar
 # "Practical Applications in R for Psychologists" 01-intro Github repo
 # https://github.com/mattansb/Practical-Applications-in-R-for-Psychologists
 
@@ -10,18 +10,18 @@
 # Results of any command will appear in the "Cell Output" below
 
 # %%
-# ASSIGN a value to an object with =
+# ASSIGN a value to a variable with =
 # (it will now appear under "VARIABLES" section)
 a = 3
 
 # %%
-# Running code with only the name of an object results in "printing" its contents:
+# Running code with only the name of a variable results in "printing" its contents:
 a
 
 # %%
-# You can use the object just as you would its value:
+# You can use the variable just as you would its value:
 a + 4
-# Note that the value of "a" is not changed, 
+# Note that the value of "a" is not changed,
 # and that the results were not saved anywhere
 
 # %%
@@ -54,40 +54,41 @@ c          # then print
 
 
  # %%
-# The variables we created above were all type "int".
-# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+# The variables we created above were all of type "int".
+# Int, or integer, is a whole number, positive or negative.
 # Let's look at other types of variables that we can use when writing a code.
 
 # %%
 # Strings:
 
-# A string is a sequence of characters (Python does not have a character data type, 
+# A string is a sequence of characters (Python does not have a character data type,
 # a single character is simply a string with a length of 1)
 
-string = "sequence of characters (letters/ numbers/ symbols)"
+my_string = "sequence of characters (letters/numbers/symbols)"
+# it is customary to name Python variables using lower case letters and underscores
 
 # %%
-Group1 = "77"
+group1 = "77"
 
 # %%
-Group2 = "experimental"
+group2 = "experimental"
 
 # %%
-Group1 / a # what's happened here?
+group1 / a # what's happened here?
 
 # %%
 # adding strings to each other
-Group3 = Group1+Group2
-print(Group3)
+group3 = group1+group2
+print(group3)
 
 # %%
-Group3 = Group1+' '+Group2
-print(Group3)
+group3 = group1 + ' ' + group2
+print(group3)
 
 # %%
-# for numbers, the + character works as a mathematical operator
+# for numerical types, the + character works as a mathematical operator
 # what will happen if we will try to combine a string and a number using +?
-new = a + Group3
+new = a + group3
 
 # %%
 # Floats:
@@ -98,12 +99,10 @@ a = 3.1
 d = c - a
 
 # %%
-# another example is needed here
-
-# %%
-# Logical:
-# TRUE/FALSE object
+# Booleans:
+# True/False object
 a > b
+print(type(a>b))
 
 # %%
 a = 7
@@ -118,14 +117,22 @@ a = a == 7
 a != 7
 
 # %%
-a < 10 & b > 8
+not a == 7
 
 # %%
-a < 10 | b > 8
+a = 7
+b = 5
+
+a < 10 and b > 8
+
+# %%
+a < 10 or b > 8
+
+# &, |, and ~ are bitwise operators, use them only when you know what you're doing
 
 # %%
 # "isinstance" function:
-# returns "True" if the specified object is of the specified type, otherwise "False"
+# returns True if the specified variable is of the specified type, otherwise False
 isinstance(a, int)
 
 # %%
@@ -139,7 +146,7 @@ type(a)
 type(a) == type(b)
 
 # %%
-# change the type of object
+# change the type of object to another type. In programming, this is called "casting"
 a = str(a)
 
 # %%
@@ -149,12 +156,12 @@ a = int(a)
 # %%
 # Operators:
 
-# (+)   addition  
-# (-)   subtraction  
-# (*)   multiplication  
-# (/)   division  
-# (^)   power  
-# (%%)  modulo. The remainder after division. E.g., 5 %% 2  gives  1  
+# (+)   addition
+# (-)   subtraction
+# (*)   multiplication
+# (/)   division
+# (^)   power
+# (%%)  modulo. The remainder after division. E.g., 5 %% 2  gives  1
 # (%/%) integer division. E.g., 7 %/% 3  gives  2
 
 # %%
@@ -166,8 +173,10 @@ a + (b / 2)
 # %%
 (a + b) / 2
 
+# While the order of operations is deterministic (and the same as in math)
+# it is a good practice to use parentheses to make your code more readable
+# and fault-tolerant.
 
-# %%
 # (lists, tuples and dictionaries code chuncks origin: https://www.educative.io/answers/list-vs-tuple-vs-set-vs-dictionary-in-python)
 
 # %%
@@ -176,7 +185,7 @@ a + (b / 2)
 
 list1=[1,4,"Be'er-Sheva",6,"five"]
 list2=[]  # creates an empty list
-list3=list((1,2,3))
+list3=list((1,2,3)) # creates a list from a tuple
 print(list1)
 print(list2)
 print(list3)
@@ -195,16 +204,31 @@ list2=["apple","mango","banana"]
 print(list1+list2) # list concatenation
 
 # %%
-# append() method adds a single item at the end of the list without modifying the original list.
+# the append() method adds a single item at the end of the list without modifying the original list.
 list1=["apple","banana","grapes"]
 print(list1)
+
+# What's the difference between a method and a function?
+# A function is a block of code that is executed when it is called.
+# A method is a function that is a part of a class.
+
+# .append() is a method of the list class. Its argument can be any object.
+# print() is a function.
 
 # %%
 list1.append("strawberry")   # strawberry is added to the list
 print(list1)
 
+list1.extend(["orange","mango","grapes"])  # another list is added to the list
+print(list1)
+
+# note that these methods does not return anything, they modify the list in place
+print(list1.append("strawberry")) # returns None
+
+
+
 # %%
-# pop() method removes the item at the given index from the list and returns it.
+# the pop() method removes the item at the given index from the list and returns it.
 list1.pop()  # removes the last element from the list
 print(list1)
 
@@ -226,6 +250,10 @@ print(tuple2)
 print(tuple3)
 
 # %%
+# Does this work?
+tuple1[0] = 2
+
+# %%
 tuple2=("orange","grapes")
 print(tuple1+tuple2)  # tuples can be concatenated
 
@@ -238,23 +266,23 @@ print(type(tuple3))
 # an unordered, changeable and indexed collection of data that stores data in key-value pairs
 dict1={"key1":"value1","key2":"value2"}
 dict2={}   # empty dictionary
-dict3=dict({1:"apple",2:"cherry",3:"strawberry"})
+favorable_fruits=dict({"John":"apple","Jane":"cherry","Dana":None})
 print(dict1)
 print(dict2)
-print(dict3)
+print(favorable_fruits)
 
 # %%
 # mutable vs. immutable variables
 
-# some variables can change their value after creation while some cannot. 
-# Therefore, if a variable can change its value it is mutable in nature. 
-# Otherwise, if it cannot change its value after creation it is immutable in nature. 
+# some variables can change their value after creation while some cannot.
+# Therefore, if a variable can change its value it is mutable in nature.
+# Otherwise, if it cannot change its value after creation it is immutable in nature.
 
-# Moreover, if we try to change the value of an immutable variable then the old variable destroys 
-# and a new variable is created with the same name and new value. 
+# Moreover, if we try to change the value of an immutable variable then the old variable is destroyed
+# and a new variable is created with the same name and new value.
 # Hence, mutable variables can change their state or content. Whereas immutable variables cannot change their state or content.
 
-# (explanation origin: https://www.toppr.com/guides/computer-science/introduction-to-python/mutable-and-immutable-variables/mutable-and-immutable-variables/)
+# (explanation source: https://www.toppr.com/guides/computer-science/introduction-to-python/mutable-and-immutable-variables/mutable-and-immutable-variables/)
 
 # %%
 # lists are mutable
@@ -320,7 +348,7 @@ a = [5]
 b = a
 
 # %%
-a[0] =7 #what will happen now?
+a[0] = 7 #what will happen now?
 
 # %%
 b
